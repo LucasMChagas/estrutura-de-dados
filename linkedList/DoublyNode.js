e6cd64b1b5b36ec807f7b67e1ebec8f5e35259fa
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2026-01-08 20:47:10 -0300

    doubly linked list class added

---
+++ linkedList/DoublyNode.js
@@ -0,0 +1,8 @@
+import Node from "./Node";
+
+export default class DoublyNode extends Node{
+    constructor(element, next, prev) {
+        super(element, next);
+        this.prev = prev;
+    }
+}
